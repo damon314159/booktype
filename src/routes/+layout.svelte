31de484f00cac35462ff014e19590d59d442abaf
Gustavo Dominguez <gfdominguez119@gmail.com>
2024-01-25 20:36:11 -0300

    Add shadcn-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
-<script>
-  import "../app.css";
+<script lang="ts">
+  import '../app.pcss';
+  import { ModeWatcher } from 'mode-watcher';
+  import ModeSwitch from '$lib/components/ui/theme/ModeSwitch.svelte';
+  import { Button } from '$lib/components/ui/button/';
 </script>
 
+<ModeWatcher />
+<ModeSwitch />
+<Button
+  on:click={() => {
+    alert('dumbass lmaoo');
+  }}
+  >Click me :)
+</Button>
 <slot />
